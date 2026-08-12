d8a76407971a9a099baaaa6fe568e8b6ee94df46
Ankita2525 <85961826+Ankita2525@users.noreply.github.com>
2026-08-12 03:03:13 -0700

    Update ask-question-card.tsx

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -51,7 +51,7 @@ const AskQuestionCard = () => {
           <DialogHeader>
             <div className="flex items-center gap-2">
               <DialogTitle>
-                <Image src="/logo.png" alt="repoverse" width={40} height={40} />
+                <Image src="/logo.png" alt="RepoVerse" width={40} height={40} />
               </DialogTitle>
               <Button
                 disabled={saveAnswer.isPending}
